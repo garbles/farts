f37cf489f350df7f0ad6205a6618231ba69676ec
Gabe Scholz <scholz.gabe@gmail.com>
2016-10-29 07:34:48 -0700

    Install standard linter, add death package

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
-const path = require('path');
-const watch = require('../src');
+const ON_DEATH = require('death')
+const watch = require('../src')
 
-const args = process.argv.slice(2);
-const watcher = watch(args[0]);
+const args = process.argv.slice(2)
+const watcher = watch(args[0])
 
 watcher.on('ready', pathName => {
-  console.log('Now watching ' + pathName);
-});
+  console.log('Now watching ' + pathName)
+})
+
+ON_DEATH(watcher.kill)
--- src/index.js
+++ src/index.js
@@ -1,84 +1,93 @@
-const EventEmitter = require('events');
-const chokidar = require('chokidar');
-const fs = require('fs');
-const glob = require('glob');
-const path = require('path');
-const template = require('lodash/template');
-const noop = require('lodash/noop');
-const through = require('through2');
-const j = require('jscodeshift');
+const EventEmitter = require('events')
+const chokidar = require('chokidar')
+const fs = require('fs')
+const glob = require('glob')
+const path = require('path')
+const template = require('lodash/template')
+const noop = require('lodash/noop')
+const through = require('through2')
+const j = require('jscodeshift')
 
-const processTemplate = custom => through.obj(function(chunk, env, cb) {
-  const config = Object.assign({}, require('lodash/string'), custom);
-  const result = template(chunk.toString())(config);
+const processTemplate = custom => through.obj(function (chunk, env, cb) {
+  const config = Object.assign({}, require('lodash/string'), custom)
+  const result = template(chunk.toString())(config)
 
-  this.push(result);
+  this.push(result)
 
-  cb();
-});
+  cb()
+})
 
-const createMissingImports = filePath => through.obj(function(chunk, env, cb) {
-  const str = chunk.toString();
+const createMissingImports = filePath => through.obj(function (chunk, env, cb) {
+  const str = chunk.toString()
 
   j(str, {parser: require('jscodeshift/parser/flow')})
     .find(j.ImportDeclaration)
     .forEach(p => {
-      const resolved = path.resolve(filePath.dir, p.get('source', 'value').value);
+      const resolved = path.resolve(filePath.dir, p.get('source', 'value').value)
       fs.access(resolved, fs.F_OK, err => {
         if (err) {
-          fs.writeFileSync(resolved, '');
+          fs.writeFileSync(resolved, '')
         }
-      });
-    });
+      })
+    })
 
-  this.push(chunk);
-  cb();
-});
+  this.push(chunk)
+  cb()
+})
 
 module.exports = (pathName = './', cb = noop) => {
-  const emitter = new EventEmitter();
-  const watcher = chokidar.watch(pathName, {ignored: /[\/\\]\./});
+  const emitter = new EventEmitter()
+  const watcher = chokidar.watch(pathName, {ignored: /[\/\\]\./})
 
   const api = {
     on: (...args) => emitter.on(...args),
-    off: (...args) => emitter.off(...args)
-  };
+    off: (...args) => emitter.off(...args),
+    kill: () => watcher.unwatch(pathName)
+  }
 
   watcher.on('ready', () => {
-    cb.call(api);
+    cb.call(api)
 
     watcher.on('add', file => {
       fs.stat(file, (err, stats) => {
-        if (stats.birthtime.getTime() !== stats.atime.getTime() || stats.size > 0) {
-          return; // file moved
+        if (err) {
+          throw err
         }
 
-        const fileURL = path.parse(file);
-        const templateGlob = path.join(fileURL.dir, `.template${fileURL.ext}`);
+        if (stats.birthtime.getTime() !== stats.atime.getTime() || stats.size > 0) {
+          return // file moved
+        }
+
+        const fileURL = path.parse(file)
+        const templateGlob = path.join(fileURL.dir, `.template${fileURL.ext}`)
 
         glob(templateGlob, (err, files) => {
-          const templatePath = files[0];
+          if (err) {
+            throw err
+          }
+
+          const templatePath = files[0]
 
           if (templatePath) {
-            const reader = fs.createReadStream(templatePath);
-            const writer = fs.createWriteStream(file);
+            const reader = fs.createReadStream(templatePath)
+            const writer = fs.createWriteStream(file)
 
             reader
               .pipe(processTemplate(fileURL))
               .pipe(createMissingImports(fileURL))
-              .pipe(writer, {end: true});
+              .pipe(writer, {end: true})
 
             reader.on('end', () => {
-              writer.end();
-              emitter.emit('data', file);
-            });
+              writer.end()
+              emitter.emit('data', file)
+            })
           }
-        });
-      });
-    });
+        })
+      })
+    })
 
-    emitter.emit('ready', pathName);
-  });
+    emitter.emit('ready', pathName)
+  })
 
-  return api;
-};
+  return api
+}
